f80fb7c4d4ef4e43b427cdb9c3eb5f44bfae78c4
Hao <billhao@gmail.com>
2014-09-13 15:54:07 -0700

    No editing (swipe to right for delete button) for buyer list page

--- food/MasterViewController.m
+++ food/MasterViewController.m
@@ -190,7 +190,7 @@
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
-    return YES;
+    return NO;
 }
 
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
