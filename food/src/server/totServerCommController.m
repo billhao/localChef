e069a2741ae4c647be4a159701a6188b39c06603
Hao <billhao@gmail.com>
2014-09-18 21:10:50 -0700

    1. fixed a bug when publishing & in text is not escaped. 2. increment to v1.2 build 6

--- food/src/server/totServerCommController.m
+++ food/src/server/totServerCommController.m
@@ -455,7 +455,8 @@
 
     NSDictionary* req = [FoodItem toDictionary:food];
     NSString* reqStr = [totUtility ObjectToJSON:req];
-    reqStr = [reqStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]]; // escape the input
+    // [reqStr stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]  ]; // does not escape &, change to the one below
+    reqStr = [self URLEncodedString_ch:reqStr];
     NSString* data = [NSString stringWithFormat:@"type=dish&data=%@&secret=%@", reqStr, global.user.secret];
     NSDictionary* resp = [self sendStr:data toURL:m_data_url returnMessage:nil];
     
@@ -592,6 +593,28 @@
     return [NSURL URLWithString: imageURLString];
 }
 
+#pragma mark - Helper functions
+
+- (NSString *) URLEncodedString_ch:(NSString*)input {
+    NSMutableString * output = [NSMutableString string];
+    const unsigned char * source = (const unsigned char *)[input UTF8String];
+    unsigned long sourceLen = strlen((const char *)source);
+    for (int i = 0; i < sourceLen; ++i) {
+        const unsigned char thisChar = source[i];
+        if (thisChar == ' '){
+            [output appendString:@"+"];
+        } else if (thisChar == '.' || thisChar == '-' || thisChar == '_' || thisChar == '~' ||
+                   (thisChar >= 'a' && thisChar <= 'z') ||
+                   (thisChar >= 'A' && thisChar <= 'Z') ||
+                   (thisChar >= '0' && thisChar <= '9')) {
+            [output appendFormat:@"%c", thisChar];
+        } else {
+            [output appendFormat:@"%%%02X", thisChar];
+        }
+    }
+    return output;
+}
+              
 @end
 
 
